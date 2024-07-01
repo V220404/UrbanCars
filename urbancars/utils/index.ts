52675ede960fb524f34d9a2ea5bd9f2f643f9edb
V220404 <sankhevarun22@gmail.com>
2024-07-01 13:32:34 +0530

    api key

--- urbancars/utils/index.ts
+++ urbancars/utils/index.ts
@@ -10,7 +10,7 @@ export async function fetchCars(){
     }
 
 
-const response =await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla' , {headers:headers});
+const response =await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3' , {headers:headers});
 
 const result = await response.json();
 
@@ -37,11 +37,11 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   
 
 
-  export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
     const url = new URL("https://cdn.imagin.studio/getimage");
     const { make, model, year } = car;
   
-    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+    url.searchParams.append('customer', 'hrjavascript-mastery');
     url.searchParams.append('make', make);
     url.searchParams.append('modelFamily', model.split(" ")[0]);
     url.searchParams.append('zoomType', 'fullscreen');
